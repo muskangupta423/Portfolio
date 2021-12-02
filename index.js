836bca7d2ef9052202f91c293422a0285c99e658
muskangupta423 <muskan.gupta_cs16@gla.ac.in>
2021-12-03 02:06:31 +0530

    Changed bg color, added projects

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ function showWorkDetail() {
     var icon = document.querySelector("#chevronIcon");
     if (workDetail.style.display === "block") {
         workDetail.style.display = "none";
-    }
-    else {
-        workDetail.style.display = "block";
-    }
-    if(icon.classList.contains("fa-chevron-down")){
-        icon.classList.remove("fa-chevron-down");
-        icon.classList.add("fa-chevron-up");
-    }
-    else {
         icon.classList.remove("fa-chevron-up");
         icon.classList.add("fa-chevron-down");
     }
+    else {
+        workDetail.style.display = "block";
+        icon.classList.remove("fa-chevron-down");
+        icon.classList.add("fa-chevron-up");
+    }
+    // if(icon.classList.contains("fa-chevron-up")){
+    //     icon.classList.remove("fa-chevron-up");
+    //     icon.classList.add("fa-chevron-down");
+    // }
+    // else {
+    //     icon.classList.remove("fa-chevron-down");
+    //     icon.classList.add("fa-chevron-up");
+    // }
   }
